97ebb9cfe30e9f16a2a77b1f6c975aed2bdc1596
[Frankie Barrios] <[softwaredevfb@gmail.com]>
2018-12-18 10:14:37 -0600

    Added progress bar for scrolling, favicon, moved title animation up and decreased container size.

--- main.js
+++ main.js
@@ -64,3 +64,15 @@ function init() {
     // Init TypeWriter
     new TypeWriter(txtElement, words, wait);
 }
+
+
+//Scrolling Progress Bar Function
+// When the user scrolls the page, execute scrollFunction 
+window.onscroll = function () { scrollFunction();};
+//Scrolling function
+function scrollFunction() {
+    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    var scrolled = (winScroll / height) * 100;
+    document.getElementById("myBar").style.width = scrolled + "%";
+}
