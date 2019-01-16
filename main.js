20ebcfbc8e4222e15f936286b3a1720379872745
[Frankie Barrios] <[softwaredevfb@gmail.com]>
2019-01-16 07:43:51 -0600

    Clean Up and Commenting

--- main.js
+++ main.js
@@ -1,23 +1,29 @@
 class TypeWriter {
     constructor(txtElement, words, wait = 3000) {
+        //Text Element that is passed in
         this.txtElement = txtElement;
+        //Word that is passed in
         this.words = words;
         this.txt = '';
+        //Which word were on in the array
         this.wordIndex = 0;
         this.wait = parseInt(wait, 10);
+        //Main method to create effect
         this.type();
+        //Boolean representing state of effect
         this.isDeleting = false;
     }
 
+    //Method to handle all effect, init on LOAD
     type() {
         // Current index of word
         const current = this.wordIndex % this.words.length;
         // Get full text of current word
         const fullTxt = this.words[current];
 
-        // Check if deleting
+        // Check if deleting, if it is, remove a character, if not add a character
         if (this.isDeleting) {
-            // Remove char
+            // Remove char using substring which removes char between indices
             this.txt = fullTxt.substring(0, this.txt.length - 1);
         } else {
             // Add char
@@ -31,12 +37,12 @@ class TypeWriter {
         let typeSpeed = 300;
 
         if (this.isDeleting) {
-            typeSpeed /= 2;
+            typeSpeed /= 2; //Speeds up delete
         }
 
-        // If word is complete
+        // If word is complete, compare by matching fulltxt of word in array
         if (!this.isDeleting && this.txt === fullTxt) {
-            // Make pause at end
+            // Makes pause at end
             typeSpeed = this.wait;
             // Set delete to true
             this.isDeleting = true;
@@ -53,7 +59,7 @@ class TypeWriter {
 }
 
 
-// Init On DOM Load
+// Init On DOM Load -- Initializes On Page Load
 document.addEventListener('DOMContentLoaded', init);
 
 // Init App
@@ -61,7 +67,7 @@ function init() {
     const txtElement = document.querySelector('.txt-type');
     const words = JSON.parse(txtElement.getAttribute('data-words'));
     const wait = txtElement.getAttribute('data-wait');
-    // Init TypeWriter
+    // Init TypeWriter, starts running all above functions
     new TypeWriter(txtElement, words, wait);
 }
 
